af8d0c66ffd4158969c6fd171e1975d23b2e0b00
Brent Cook <busterb@php.net>
2002-07-06 22:57:03 +0000

    changed license to LGPL; bumped version in preparation for initial release
    git-svn-id: http://svn.php.net/repository/pear/packages/DBA/trunk@87905 c90b9560-bf6c-de11-be94-00142212c4b1

--- DBA_Table.php
+++ DBA_Table.php
@@ -1,19 +1,23 @@
 <?php
 /* vim: set expandtab tabstop=4 shiftwidth=4: */
 // +----------------------------------------------------------------------+
-// | PHP Version 4                                                        |
+// | Copyright (c) 2002 Brent Cook                                        |
 // +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2002 The PHP Group                                |
+// | This library is free software; you can redistribute it and/or        |
+// | modify it under the terms of the GNU Lesser General Public           |
+// | License as published by the Free Software Foundation; either         |
+// | version 2.1 of the License, or (at your option) any later version.   |
+// |                                                                      |
+// | This library is distributed in the hope that it will be useful,      |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    |
+// | Lesser General Public License for more details.                      |
+// |                                                                      |
+// | You should have received a copy of the GNU Lesser General Public     |
+// | License along with this library; if not, write to the Free Software  |
+// | Foundation, Inc., 59 Temple Place, Suite 330,Boston,MA 02111-1307 USA|
 // +----------------------------------------------------------------------+
-// | This source file is subject to version 2.02 of the PHP license,      |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available at through the world-wide-web at                           |
-// | http://www.php.net/license/2_02.txt.                                 |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Authors: Brent Cook <busterb@mail.utexas.edu>                        |
+// | Author: Brent Cook <busterb@mail.utexas.edu>                         |
 // +----------------------------------------------------------------------+
 //
 // $Id$
@@ -39,7 +43,7 @@ define('DBA_FIELD_SEPARATOR', '|');
  * It uses a DBA class as the storage driver.
  *
  * @author Brent Cook <busterb@mail.utexas.edu>
- * @version 0.0.12
+ * @version 0.0.14
  */
 class DBA_Table extends PEAR
 {
