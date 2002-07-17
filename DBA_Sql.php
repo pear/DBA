180f57d3ac20d3f4f0c906a3d893fa6d256d862b
Brent Cook <busterb@php.net>
2002-07-17 07:28:30 +0000

    moved sql bits to DBA_Sql, added 'insert' to the repetoir
    git-svn-id: http://svn.php.net/repository/pear/packages/DBA/trunk@88887 c90b9560-bf6c-de11-be94-00142212c4b1

--- DBA_Sql.php
+++ DBA_Sql.php
@@ -21,193 +21,74 @@
 // $Id$
 //
 
-require_once "PEAR.php";
-require_once "Sql_lex.php";
+require_once 'PEAR.php';
+require_once 'DB/DBA/Sql_parse.php';
+require_once 'DB/DBA/DBA_Relational.php';
 
-class Sql
-{
+    function executeSql(&$db, $filename) {
+        $sqlfile = implode('', file($filename));
+        $parser = new Sql_Parser($sqlfile);
+        $typeMap = array(
+            SQL_INT=>DBA_INTEGER,
+            SQL_FIXED=>DBA_FIXED,
+            SQL_FLOAT=>DBA_FLOAT,
+            SQL_CHAR=>DBA_CHAR,
+            SQL_VARCHAR=>DBA_VARCHAR,
+            SQL_TEXT=>DBA_TEXT,
+            SQL_BOOL=>DBA_BOOLEAN,
+            SQL_ENUM=>DBA_ENUM,
+            SQL_SET=>DBA_SET,
+            SQL_TIMESTAMP=>DBA_TIMESTAMP,
+            SQL_DATE=>DBA_TIMESTAMP,
+            SQL_TIME=>DBA_TIMESTAMP
+        );
 
-// adds spaces around special characters
-function cookQuery($query)
-{
-    return str_replace (
-        array(',','(',')',"\'","\\\""),
-        array(' , ',' ( ',' ) '," \' "," \\\" "),
-        $query);
-}
+        $optionMap = array(
+            SQL_SIZE=>DBA_SIZE,
+            SQL_DEFAULT=>DBA_DEFAULT,
+            SQL_NOTNULL=>DBA_NOTNULL,
+            SQL_DOMAIN=>DBA_DOMAIN,
+            SQL_AUTOINCREMENT=>DBA_AUTOINCREMENT,
+        );
 
-function getToken()
-{
-    $this->lexer->lex;
-    return $this->lexer->currTok;
-//    return strtok(" \n\t");
-}
-
-function getString() {
-    $string = strtok(" \n\t");
-    $quote = substr($string, 0, 1);
-    $endquote = ($quote == '(') ? ')' : $quote;
-
-    if (($quote != '"') && ($quote != "'") && ($quote != '(')) {
-        // this is a bare string, just return the token
-        return $string;
-
-    } elseif ((substr($string, -1) == $endquote) &&
-              (substr($string, -2) != "\\$endquote") &&
-              (strlen($string) > 1)) {
-        // corner case of a single quoted word
-        return substr($string, 1, -1);
-    } else {
-        // strip the first quote, add missing character
-        $string = substr($string, 1).' ';
-        // read until the next quote
-        $string .= strtok($endquote);
-        // was this an escaped quote?
-        while (substr($string, -1) == "\\") {
-            // insert the quote, keep reading
-            $string = substr($string,0,-2).$endquote.substr(strtok($endquote), 1);
-        }
-        return $string;
-    }
-}
-
-function parseCreate($rawquery)
-{
-//    $query = $this->cookQuery($rawquery);
-    $command = ;
-
-    if ($command != 'create') {
-        return PEAR::raiseError('DBA: not a "create" query');
-    }
-    
-    $thing = $this->getToken();
-    if ($thing == 'table') {
-        $tableName = $this->getToken();
-        $tableDefn =& $this->parseTableDefinition();
-        return array($tableName, $tableDefn);
-    } else {
-        return PEAR::raiseError("Do not know how to create $thing");
-    }
-}
-
-function &parseTableDefinition()
-{
-    $token = $this->getToken();
-    if ($token != '(') {
-        return PEAR::raiseError('No field definitions');
-    } else {
-        while ($token && ($token != ')')) {
-
-            // check if we're at the end of our rope, otherwise
-            // it's another day at the office
-            if (($token = SQL::getToken()) == ')') {
-                break;
-            }
-            $fieldName = $token;
-
-            $table[$fieldName]['type'] = $fieldType = $this->getToken();
-
-            // parse field-specific parameters
-            $token = $this->getToken();
-            if ($token == '(') {
-                $element = 0;
-                while ($token && ($token != ')')) {
-                    $table[$fieldName]['opts'][$element] = $this->getString();
-                    $token = $this->getToken();
-                    if ($token == ',') {
-                        ++$element;
-                    } else
-                    if ($token != ')') {
-                        return PEAR::raiseError("Expected ')', got '$token' on '$fieldName'");
-                    }
-                }
-                $token = $this->getToken();
-            }
-
-            // parse extra options
-            while ($token && !(($token == ',') || ($token == ')'))){
-                switch ($token) {
-                    case 'constraint':
-                        $table[$fieldName]['constraint']['name'] = SQL::getToken();
-                        $nextConstraint = true;
-                        $haveValue = false;
-                        $token = $this->getToken();
-                        break;
-                    case 'default':
-                        $option = $token;
-                        $value = $this->getString();
-                        $haveValue = true;
-                        $token = $this->getToken();
-                        if ($token == '(') {
-                            $token = strtok(')');
-                            $value = $value.'('.$token.')';
+        while ($parser->token != TOK_END_OF_INPUT) {
+            $tree = $parser->parse();
+            if (is_null($tree)) {
+                return;
+            } elseif (PEAR::isError($tree)) {
+                return $tree;
+            } else {
+                switch ($tree[SQL_COMMAND]) {
+                    case SQL_CREATE_TABLE:
+                        $name = $tree[SQL_NAME];
+                        $schema = array();
+                        foreach ($tree[SQL_FIELDS] as $field) {
+                            $fieldName = $field[SQL_NAME];
+                            $schema[$fieldName][DBA_TYPE] = $typeMap[$field[SQL_TYPE]];
+                            
+                            foreach ($optionMap as $sql=>$dba) {
+                                if (isset($field[$sql]))
+                                    $schema[$fieldName][$dba] = $field[$sql];
+                            }
                         }
-                        break;
-                    case 'primary':
-                        $token = $this->getToken();
-                        if ($token != 'key') {
-                            return PEAR::raiseError(
-                                "Expected 'key', got '$token' on '$fieldName'");
-                        } else {
-                            $option = 'primary key';
-                            $value = true;
-                            $haveValue = true;
+                        if (!$db->tableExists($name)) {
+                            $result = $db->createTable($name, $schema);
+                            if (PEAR::isError($result)) {
+                                return $result;
+                            }
                         }
-                        $token = $this->getToken();
-                        break;
-                    case 'not':
-                        $token = $this->getToken();
-                        if ($token != 'null') {
-                            return PEAR::raiseError(
-                                "Expected 'null', got '$token' on '$fieldName'");
-                        } else {
-                            $option = 'not null';
-                            $value = true;
-                            $haveValue = true;
+                    break;
+                    case SQL_INSERT:
+                        foreach ($tree[SQL_FIELDS] as $key=>$field) {
+                            $row[$field] = $tree[SQL_VALUES][$key];
                         }
-                        $token = $this->getToken();
-                        break;
-                    case 'check': case 'varying': case 'unique':
-                        $option = $token;
-                        $value = $this->getString();
-                        $haveValue = true;
-                        $token = $this->getToken();
-                        break;
-                    case 'year':case'month':case 'day':case 'hour':case 'minute':case 'second':
-                        $option = 'duration';
-                        $table[$fieldName]['from'] = $token;
-                        $token = $this->getToken();
-                        if ($token != 'to') {
-                            return PEAR::raiseError(
-                                "Expected 'to', got $token on '$fieldName'");
+                        $db->insert($name, $row);
+                        if (PEAR::isError($result)) {
+                            return $result;
                         }
-                        $table[$fieldName]['to'] = $this->getToken();
-                        $haveValue = false;
-                        $token = $this->getToken();
-                        break;
-                    default:
-                        $option = $token;
-                        $value = true;
-                        $haveValue = true;
-                        $token = $this->getToken();
-                        break;
-                }
-                if ($haveValue) {
-                    if ($nextConstraint) {
-                        $table[$fieldName]['constraint'][$option] = $value;
-                        $nextConstraint = false;
-                    } else {
-                        $table[$fieldName][$option] = $value;
-                    }
+                    break;
                 }
             }
         }
-        if ($token != ')') {
-            echo ("Unexpected end of input with $token\n");
-        }
-        return $table;
     }
-}
-
-}
 ?>
--- DBA_Toolbox.php
+++ DBA_Toolbox.php
@@ -20,10 +20,7 @@
 //
 // $Id$
 //
-require_once 'DB/DBA/DBA_Table.php';
 require_once 'PEAR.php';
-require_once 'DB/DBA/Sql_parse.php';
-require_once 'DB/DBA/DBA_Relational.php';
 
 /**
  * Insert widgets into a quickform object suitable for updating a row in a DBA
@@ -140,64 +137,5 @@ function formatTextTable($rows, $fields = null, $style = 'oracle')
         }
     }
     return $buffer;
-}
-
-function executeSqlSchema(&$db, $filename) {
-    $sqlfile = implode('', file($filename));
-    $parser = new Sql_Parser($sqlfile);
-    $tokenMap = array(
-        SQL_SIZE=>DBA_SIZE,
-        SQL_DEFAULT=>DBA_DEFAULT,
-        SQL_NOTNULL=>DBA_NOTNULL,
-        SQL_DOMAIN=>DBA_DOMAIN,
-        SQL_INT=>DBA_INTEGER,
-        SQL_FIXED=>DBA_NUMERIC,
-        SQL_FLOAT=>DBA_FLOAT,
-        SQL_CHAR=>DBA_CHAR,
-        SQL_VARCHAR=>DBA_VARCHAR,
-        SQL_TEXT=>DBA_TEXT,
-        SQL_BOOL=>DBA_BOOLEAN,
-        SQL_ENUM=>DBA_ENUM,
-        SQL_SET=>DBA_SET,
-        SQL_TIMESTAMP=>DBA_TIMESTAMP,
-        SQL_DATE=>DBA_TIMESTAMP,
-        SQL_TIME=>DBA_TIMESTAMP
-    );
-
-    while (1) {
-        $tree = $parser->parse();
-        if (PEAR::isError($tree)) {
-            if ($parser->token == TOK_END_OF_INPUT) {
-                return;
-            } else {
-                return $tree;
-            }
-        } else {
-            switch ($tree[SQL_COMMAND]) {
-                case SQL_CREATE_TABLE:
-                    $name = $tree[SQL_NAME];
-                    $schema = array();
-                    foreach ($tree[SQL_FIELDS] as $field) {
-                        $fieldName = $field[SQL_NAME];
-                        $schema[$fieldName][DBA_TYPE] = $tokenMap[$field[SQL_TYPE]];
-                        if ($field[SQL_SIZE])
-                            $schema[$fieldName][DBA_SIZE] = $field[SQL_SIZE];
-                        if ($field[SQL_DEFAULT])
-                            $schema[$fieldName][DBA_DEFAULT] = $field[SQL_DEFAULT];
-                        if ($field[SQL_NOTNULL])
-                            $schema[$fieldName][DBA_NOTNULL] = $field[SQL_NOTNULL];
-                        if ($field[SQL_DOMAIN])
-                            $schema[$fieldName][DBA_DOMAIN] = $field[SQL_DOMAIN];
-                    }
-                if (!$db->tableExists($name)) {
-                    $result = $db->createTable($name, $schema);
-                    if (PEAR::isError($result)) {
-                        return $result;
-                    }
-                }
-                break;
-            }
-        }
-    }
 }
 ?>
