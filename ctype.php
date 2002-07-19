6c22319dbde8950c45ece22561b9ade76eaf4caf
Brent Cook <busterb@php.net>
2002-07-19 21:02:33 +0000

    optimize a little more, after this morning's exercise in creative fortune-telling
    git-svn-id: http://svn.php.net/repository/pear/packages/DBA/trunk@89108 c90b9560-bf6c-de11-be94-00142212c4b1

--- ctype.php
+++ ctype.php
@@ -1,58 +1,59 @@
 <?php
 /**
- * PHP ctype compatibility functions. See the ctype module for more information
- * on usage.
+ * PHP ctype compatibility functions. See the PHP ctype module for more
+ * information on usage.
  *
  * @author John Millaway
  * @author Brent Cook
  * 
- * Note: These functions expect an integer character, like the 
- *
+ * Note: These functions expect an integer argument, like the C versions
+ * To use with a PHP character, use ord($c). These functions do not support
+ * string arguments like their PHP extension counterparts
  */
 if (!extension_loaded('ctype')) {
     function ctype_alnum($c) {
         global $ctype__;
-        return !($ctype__[$c] & (01 | 02 | 04));
+        return ($ctype__[$c] & 7); // (1 | 2 | 4)
     }
     function ctype_alpha($c) {
         global $ctype__;
-        return !($ctype__[$c] & (01 | 02));
+        return ($ctype__[$c] & 3); // (1 | 2)
     }
     function ctype_cntrl($c) {
         global $ctype__;
-        return !($ctype__[$c] & 040);
+        return ($ctype__[$c] & 40);
     }
     function ctype_digit($c) {
         global $ctype__;
-        return !($ctype__[$c] & 04);
+        return ($ctype__[$c] & 4);
     }
     function ctype_graph($c) {
         global $ctype__;
-        return !($ctype__[$c] & (020 | 01 | 02 | 04));
+        return ($ctype__[$c] & 27); // (20 | 1 | 2 | 4)
     }
     function ctype_lower($c) {
         global $ctype__;
-        return !($ctype__[$c] & 02);
+        return ($ctype__[$c] & 2);
     }
     function ctype_print($c) {
         global $ctype__;
-        return !($ctype__[$c] & (020 | 01 | 02 | 04 | 0200));
+        return ($ctype__[$c] & 227); // (20 | 1 | 2 | 4 | 200)
     }
     function ctype_punct($c) {
         global $ctype__;
-        return !($ctype__[$c] & 020);
+        return ($ctype__[$c] & 20);
     }
     function ctype_space($c) {
         global $ctype__;
-        return !($ctype__[$c] & 010);
+        return ($ctype__[$c] & 10);
     }
     function ctype_upper($c) {
         global $ctype__;
-        return !($ctype__[$c] & 01);
+        return ($ctype__[$c] & 1);
     }
     function ctype_xdigit($c) {
         global $ctype__;
-        return !($ctype__[$c] & (0100 | 04));
+        return ($ctype__[$c] & 104); // (100 | 4));
     }
     $ctype__ =
     array(32,32,32,32,32,32,32,32,32,40,40,40,40,40,32,32,32,32,32,32,32,32,32,
