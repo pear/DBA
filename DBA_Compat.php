3bdae67a2eafc8c0ca16c1131750240f887817ea
Martin Jansen <mj@php.net>
2002-07-31 12:19:01 +0000

    * untabify
    git-svn-id: http://svn.php.net/repository/pear/packages/DBA/trunk@90525 c90b9560-bf6c-de11-be94-00142212c4b1

--- DBA_Compat.php
+++ DBA_Compat.php
@@ -40,63 +40,63 @@ if (!function_exists('dba_open')) {
     require_once 'PEAR.php';
     require_once 'DB_DBA/DBA_Simple.php';
 
-	function dba_close(&$dba)
-	{
-		$result = !PEAR::isError($dba->close());
+    function dba_close(&$dba)
+    {
+        $result = !PEAR::isError($dba->close());
         unset($dba);
         return $result;
-	}
+    }
 
-	function dba_delete($key, &$dba)
-	{
-		return !PEAR::isError($dba->remove($key));
-	}
+    function dba_delete($key, &$dba)
+    {
+        return !PEAR::isError($dba->remove($key));
+    }
 
-	function dba_exists($key, &$dba)
-	{
-		return !PEAR::isError($dba->exists($key));
-	}
+    function dba_exists($key, &$dba)
+    {
+        return !PEAR::isError($dba->exists($key));
+    }
 
-	function dba_fetch($key, &$dba)
-	{
+    function dba_fetch($key, &$dba)
+    {
         return !PEAR::isError($dba->fetch($key));
-	}
+    }
 
-	function dba_firstkey(&$dba)
-	{
-		return $dba->firstkey();
-	}
+    function dba_firstkey(&$dba)
+    {
+        return $dba->firstkey();
+    }
 
-	function dba_insert($key, $value, &$dba)
-	{
-		return !PEAR::isError($dba->insert($key, $value));
-	}
+    function dba_insert($key, $value, &$dba)
+    {
+        return !PEAR::isError($dba->insert($key, $value));
+    }
 
-	function dba_nextkey(&$dba)
-	{
-		return $dba->nextkey($key);
-	}
+    function dba_nextkey(&$dba)
+    {
+        return $dba->nextkey($key);
+    }
 
-	function dba_open($filename, $mode, $handler)
-	{
-		$dba = new DBA_Simple();
-		$dba->open($filename, $mode);
+    function dba_open($filename, $mode, $handler)
+    {
+        $dba = new DBA_Simple();
+        $dba->open($filename, $mode);
         if (PEAR::isError($dba)) {
             return false;
         } else {
             return $dba;
         }
-	}
+    }
 
     function dba_popen(&$dba)
     {
         return FALSE;
     }
 
-	function dba_replace($key, $value, &$dba)
-	{
-		return !PEAR::isError($dba->replace($key, $value));
-	}
+    function dba_replace($key, $value, &$dba)
+    {
+        return !PEAR::isError($dba->replace($key, $value));
+    }
 
     function dba_sync(&$dba)
     {
