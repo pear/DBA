42f5d26de17bcbba9c0902ed32e1549f625e807b
Brent Cook <busterb@php.net>
2002-08-28 15:13:45 +0000

    executeSql now takes a string rather than a filename as input
    git-svn-id: http://svn.php.net/repository/pear/packages/DBA/trunk@93945 c90b9560-bf6c-de11-be94-00142212c4b1

--- DBA_Sql.php
+++ DBA_Sql.php
@@ -32,70 +32,70 @@ require_once 'PEAR.php';
 require_once 'DB/DBA/Sql_parse.php';
 require_once 'DB/DBA/DBA_Relational.php';
 
-    function executeSql(&$db, $filename) {
-        $sqlfile = implode('', file($filename));
-        $parser = new Sql_Parser($sqlfile);
-        $typeMap = array(
-            SQL_INT=>DBA_INTEGER,
-            SQL_FIXED=>DBA_FIXED,
-            SQL_FLOAT=>DBA_FLOAT,
-            SQL_CHAR=>DBA_CHAR,
-            SQL_VARCHAR=>DBA_VARCHAR,
-            SQL_TEXT=>DBA_TEXT,
-            SQL_BOOL=>DBA_BOOLEAN,
-            SQL_ENUM=>DBA_ENUM,
-            SQL_SET=>DBA_SET,
-            SQL_TIMESTAMP=>DBA_TIMESTAMP,
-            SQL_DATE=>DBA_TIMESTAMP,
-            SQL_TIME=>DBA_TIMESTAMP
-        );
+function executeSql(&$db, $sqlText) {
+    $parser = new Sql_Parser($sqlText);
 
-        $optionMap = array(
-            SQL_SIZE=>DBA_SIZE,
-            SQL_DEFAULT=>DBA_DEFAULT,
-            SQL_NOTNULL=>DBA_NOTNULL,
-            SQL_DOMAIN=>DBA_DOMAIN,
-            SQL_AUTOINCREMENT=>DBA_AUTOINCREMENT,
-        );
+    $typeMap = array(
+        SQL_INT=>DBA_INTEGER,
+        SQL_FIXED=>DBA_FIXED,
+        SQL_FLOAT=>DBA_FLOAT,
+        SQL_CHAR=>DBA_CHAR,
+        SQL_VARCHAR=>DBA_VARCHAR,
+        SQL_TEXT=>DBA_TEXT,
+        SQL_BOOL=>DBA_BOOLEAN,
+        SQL_ENUM=>DBA_ENUM,
+        SQL_SET=>DBA_SET,
+        SQL_TIMESTAMP=>DBA_TIMESTAMP,
+        SQL_DATE=>DBA_TIMESTAMP,
+        SQL_TIME=>DBA_TIMESTAMP
+    );
 
-        while ($parser->token != TOK_END_OF_INPUT) {
-            $tree = $parser->parse();
-            if (is_null($tree)) {
-                return;
-            } elseif (PEAR::isError($tree)) {
-                return $tree;
-            } else {
-                switch ($tree[SQL_COMMAND]) {
-                    case SQL_CREATE_TABLE:
-                        $name = $tree[SQL_NAME];
-                        $schema = array();
-                        foreach ($tree[SQL_FIELDS] as $field) {
-                            $fieldName = $field[SQL_NAME];
-                            $schema[$fieldName][DBA_TYPE] = $typeMap[$field[SQL_TYPE]];
-                            
-                            foreach ($optionMap as $sql=>$dba) {
-                                if (isset($field[$sql]))
-                                    $schema[$fieldName][$dba] = $field[$sql];
-                            }
+    $optionMap = array(
+        SQL_SIZE=>DBA_SIZE,
+        SQL_DEFAULT=>DBA_DEFAULT,
+        SQL_NOTNULL=>DBA_NOTNULL,
+        SQL_DOMAIN=>DBA_DOMAIN,
+        SQL_AUTOINCREMENT=>DBA_AUTOINCREMENT,
+    );
+
+    while ($parser->token != TOK_END_OF_INPUT) {
+        $tree = $parser->parse();
+        if (is_null($tree)) {
+            return;
+        } elseif (PEAR::isError($tree)) {
+            return $tree;
+        } else {
+            switch ($tree[SQL_COMMAND]) {
+                case SQL_CREATE_TABLE:
+                    $name = $tree[SQL_NAME];
+                    $schema = array();
+                    foreach ($tree[SQL_FIELDS] as $field) {
+                        $fieldName = $field[SQL_NAME];
+                        $schema[$fieldName][DBA_TYPE] = $typeMap[$field[SQL_TYPE]];
+                        
+                        foreach ($optionMap as $sql=>$dba) {
+                            if (isset($field[$sql]))
+                                $schema[$fieldName][$dba] = $field[$sql];
                         }
-                        if (!$db->tableExists($name)) {
-                            $result = $db->createTable($name, $schema);
-                            if (PEAR::isError($result)) {
-                                return $result;
-                            }
-                        }
-                    break;
-                    case SQL_INSERT:
-                        foreach ($tree[SQL_FIELDS] as $key=>$field) {
-                            $row[$field] = $tree[SQL_VALUES][$key];
-                        }
-                        $db->insert($name, $row);
+                    }
+                    if (!$db->tableExists($name)) {
+                        $result = $db->createTable($name, $schema);
                         if (PEAR::isError($result)) {
                             return $result;
                         }
-                    break;
-                }
+                    }
+                break;
+                case SQL_INSERT:
+                    foreach ($tree[SQL_FIELDS] as $key=>$field) {
+                        $row[$field] = $tree[SQL_VALUES][$key];
+                    }
+                    $db->insert($name, $row);
+                    if (PEAR::isError($result)) {
+                        return $result;
+                    }
+                break;
             }
         }
     }
+}
 ?>
