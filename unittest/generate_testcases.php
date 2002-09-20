642364b6bbaf1106a988cbfba8e40a37d162996b
Brent Cook <busterb@php.net>
2002-09-20 14:00:31 +0000

    separate test cases into separate files
    git-svn-id: http://svn.php.net/repository/pear/packages/DBA/trunk@96277 c90b9560-bf6c-de11-be94-00142212c4b1

--- unittest/generate_testcases.php
+++ unittest/generate_testcases.php
@@ -26,68 +26,8 @@
 // test functionality of the sql parser
 
 require_once 'PEAR.php';
-require_once '../Sql_parse.php';
-
-$queries = array(
-"CREATE TABLE albums (
-  name varchar(60),
-  directory varchar(60),
-  rating enum (1,2,3,4,5,6,7,8,9,10) NOT NULL,
-  category set('sexy','\'family time\'',\"outdoors\",'generic','very weird') NULL,
-  description text NULL,
-  id int default 200 PRIMARY KEY
-)",
-"CREATE TABLE photos (
-  filename varchar(60) not NULL,
-  name varchar(60) default \"no name\",
-  album int,
-  price float (4,2),
-  description text default 'hello',
-  id int default 0 primary key not null,
-)",
-"create table brent (
-    filename varchar(10),
-    description varchar(20),
-)",
-"create table nodefinitions",
-"create dogfood",
-"create table dunce (name varchar",
-"create table dunce (name varchar(2,3))",
-"create table dunce (enum)",
-"create table dunce (enum(23))",
-"CREATE TABLE films ( 
-             code      CHARACTER(5) CONSTRAINT firstkey PRIMARY KEY, 
-             title     CHARACTER VARYING(40) NOT NULL, 
-             did       DECIMAL(3) NOT NULL, 
-             date_prod DATE, 
-             kind      CHAR(10), 
-             len       INTERVAL HOUR TO MINUTE
-)",
-//             CONSTRAINT production UNIQUE(date_prod)
-"CREATE TABLE distributors ( 
-             did      DECIMAL(3) PRIMARY KEY DEFAULT NEXTVAL('serial'), 
-             name     VARCHAR(40) NOT NULL CHECK (name <> '') 
-)",
-//             CONSTRAINT con1 CHECK (did > 100 AND name > '') 
-"CREATE TABLE distributors ( 
-            did      DECIMAL(3) PRIMARY KEY, 
-            name     VARCHAR(40) 
-)",
-"insert into dogmeat ('horse', 'hair') values (2, 4)",
-"inSERT into dogmeat (horse, hair) values (2, 4)",
-"CREATE TABLE msgs ( user_id integer, msg_id integer, msg_text varchar, msg_title varchar(30), msg_date time)",
-"update dogmeat set horse=2 dog='forty' where moose <> 'howdydoo'",
-"update dogmeat set horse=2 dog='forty' where moose != 'howdydoo'",
-"update dogmeat set horse=2 , dog='forty' where moose <> 'howdydoo'",
-"update table1 set col=1 where not col = 2",
-"update table2 set col=1 where col > 2 and col <> 4",
-"update table2 set col=1 where col > 2 and col <> 4 or dog=\"Hello\"",
-"update table3 set col=1 where col > 2 and col < 30",
-"select * from dog where cat <> 4",
-"select legs, hairy from dog",
-"select max(length) from dog",
-"SELECT COUNT(DISTINCT country) FROM publishers"
-);
+require_once 'DB/DBA/Sql_parse.php';
+require_once 'Console/Getopt.php';
 
 // based on DataDumper by Matt Durell <matt@specialsource.net>
 function dump($input, $tab = "\t", $newline = "\n",
@@ -126,24 +66,55 @@ function dump($input, $tab = "\t", $newline = "\n",
     }
 }
 
-$lexer = new Lexer();
-
 $parser = new Sql_Parser();
+
+$progname = basename(array_shift($argv));
+
+$options = Console_Getopt::getopt($argv, "f:");
+if (PEAR::isError($options)) {
+    usage($options);
+}
+
+$opts = $options[0];
+foreach ($opts as $opt) {
+    switch ($opt[0]) {
+        case 'f':
+            $file = $opt[1];
+            break;
+        default:
+            echo "Unknown option :".$opt[0];
+            exit;
+    }
+}
+
+if (!$fd = @fopen($file, 'r')) {
+    echo("Could not load the SQL source file: $file\n");
+    exit;
+}
+$source = '';
+while ($data = fread($fd, 2048)) {
+    $source .= $data;
+}
+fclose($fd);
+
+$queries = explode(";\n", $source);
 
 echo "<?php\n";
 echo '$tests = array('."\n";
 
 foreach ($queries as $query) {
-    echo "array(\n";
-    echo "'sql' => '".preg_replace("/([\'\\\])/", "\\\\\\1", $query)."',\n";
-    $results = $parser->parse($query);
-    echo "'expect' => ";
-    if (PEAR::isError($results)) {
-        echo "'".preg_replace("/([\'\\\])/", "\\\\\\1", $results->getMessage())."'\n";
-    } else {
-        echo dump($results, '    ', "\n", ' ', '    ');
+    if ($query) {
+        echo "array(\n";
+        echo "'sql' => '".preg_replace("/([\'\\\])/", "\\\\\\1", $query)."',\n";
+        $results = $parser->parse($query);
+        echo "'expect' => ";
+        if (PEAR::isError($results)) {
+            echo "'".preg_replace("/([\'\\\])/", "\\\\\\1", $results->getMessage())."'\n";
+        } else {
+            echo dump($results, '    ', "\n", ' ', '    ');
+        }
+        echo "\n),\n";
     }
-    echo "\n),\n";
 }
 
 echo ");\n";
