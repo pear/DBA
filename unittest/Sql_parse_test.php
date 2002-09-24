17c24b584b8af3f93748d9a4d650b4e576509e4e
Brent Cook <busterb@php.net>
2002-09-24 20:09:23 +0000

    these moved to the SQL_Parser package
    git-svn-id: http://svn.php.net/repository/pear/packages/DBA/trunk@96736 c90b9560-bf6c-de11-be94-00142212c4b1

--- unittest/Sql_parse_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'Sql_parse_testcases.php';
-require_once 'PHPUnit/PHPUnit.php';
-
-$suite = new PHPUnit_TestSuite('SqlParserTest');
-$result = PHPUnit::run($suite);
-
-echo $result->toString();
-?>
